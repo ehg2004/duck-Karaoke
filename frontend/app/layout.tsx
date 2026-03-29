2f108f8b3feea2f1b1ebf0235dc09d09fe87d58f
Bernardo <bernardo.v.mendes@gmail.com>
2026-03-29 21:10:24 +0200

    fix: change font back to fredoka

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
       lang="en"
       className={`${fredoka.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col font-sans">{children}</body>
     </html>
   );
 }
